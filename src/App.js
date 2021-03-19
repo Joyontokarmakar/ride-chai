32c4a0120d2a0d94e6c838c93660b96cf3c2c14e
Joyonto Karmakar <41202398+Joyontokarmakar@users.noreply.github.com>
2021-03-19 11:31:09 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Home from './components/Home/Home';
+import NoMatch from './components/NoMatch/NoMatch';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/home">
+          <Home/>
+        </Route>
+
+        {/* <Route path="/club-detail/:idTeam">
+        </Route> */}
+
+        <Route exact path="/">
+          <Home/>
+        </Route>
+
+        <Route path="*">
+          <NoMatch/>
+        </Route>
+        
+      </Switch>
+    </Router>
   );
 }
 
